faebdc6ed12bee80dc81f37c97d2609fda1f7b37
Marios Dimitropoulos <dimitropoulosmarios@hotmail.com>
2019-10-11 04:53:17 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,8 +1,8 @@
 def countdown_with_sleep
   counter = 0
   while counter < 10
-  counter sleep 2.seconds 
+  counter sleep 1.seconds 
   puts "#{count} SECOND(S)!"
-  
+  puts "HAPPY NEW YEAR!"
 def countdown
 end
